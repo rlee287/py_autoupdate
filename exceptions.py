9fc33ee65e107137b8237fe2689f58afb69fed9d
rlee287 <rlee287@yahoo.com>
2016-03-02 13:01:52 -0800

    Created starting files

---
+++ exceptions.py
@@ -0,0 +1,5 @@
+class UpdaterException (Exception):
+    pass
+
+class StartUpdateException (UpdaterException):
+    pass
---
+++ launcher.py
@@ -0,0 +1,9 @@
+import requests
+import multiprocess
+from exceptions import *
+
+class Launcher:
+    def __init__(self, url)
+        self.url=url
+    def run(self):
+        pass
