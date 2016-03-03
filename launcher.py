8a665f8bb369996e488d0ce62f6a503d59ca6a02
rlee287 <rlee287@yahoo.com>
2016-03-03 12:43:11 -0800

    Added update method

--- launcher.py
+++ launcher.py
@@ -3,7 +3,9 @@ import multiprocess
 from exceptions import *
 
 class Launcher:
-    def __init__(self, url)
+    def __init__(self, url, filename, *args, **kwargs)
         self.url=url
     def run(self):
+        pass
+    def update(self):
         pass
