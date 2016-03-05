8af0718be40d9ae18d088c0ffae06bc732697167
rlee287 <rlee287@yahoo.com>
2016-03-04 21:30:28 -0800

    Created test files

---
+++ testing/test_import.py
@@ -0,0 +1,22 @@
+import sys
+
+class TestImportModules:
+    def import_module(module_name):
+        try:
+            print("Importing {}".format(module_name))
+            __import__(module_name)
+            print("Imported {}".format(module_name))
+        except:
+            raise AssertionError("Unable to import {}".format(module_name))
+
+    def import_all():
+        module_name='py_autoupdate'
+        submodules=[''];
+        modulelist=list();
+        for submodule in submodules:
+            modulelist.append(module_name+submodule)
+        for module in modulelist:
+            # No need to raise error here
+            # It is already raised in the import_module function
+            __import__(module)
+
