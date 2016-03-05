de22aa71a60eb0a83b5f972d0f18d57e685c86f5
rlee287 <rlee287@yahoo.com>
2016-03-04 22:42:56 -0800

    Added basic launcher unit

--- launcher.py
+++ launcher.py
@@ -1,11 +1,17 @@
 import requests
-import multiprocess
+import multiprocessing
+import src.run
 from exceptions import *
 
 class Launcher:
-    def __init__(self, url, filename, *args, **kwargs)
+    def __init__(self, url)
         self.url=url
+
     def run(self):
-        pass
+        p = multiprocessing.Process(target=src.run.run())
+        p.start()
+        p.join()
+
     def update(self):
         pass
+
---
+++ src/run.py
@@ -0,0 +1,5 @@
+# This is the file that is called by the updater to launch the program
+# Initialization code goes under here in the run() method
+
+def run():
+    pass
