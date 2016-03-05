404243d57f5799a9bfac9cb08067088af77dbffa
rlee287 <rlee287@yahoo.com>
2016-03-05 12:29:16 -0800

    Allow kwargs to subprocess

--- launcher.py
+++ launcher.py
@@ -4,13 +4,15 @@ from .src import start
 from .exceptions import *
 
 class Launcher:
-    def __init__(self, url):
+    def __init__(self, url, **kwargs):
         self.url=url
         self.updateEvent=multiprocessing.Event()
+        self.extraArgs=kwargs
 
     def run(self):
         p = multiprocessing.Process(target=start.run,
-                                    args=(self.updateEvent,))
+                                    args=(self.updateEvent,)
+                                    kwargs=self.extraArgs)
         p.start()
         p.join()
 
