72ac2b32cd1bf0d274b8588a7109e85dd1e2d1f4
rlee287 <rlee287@yahoo.com>
2016-03-06 13:38:13 -0800

    Updated run unit test

--- testing/test_run.py
+++ testing/test_run.py
@@ -1,6 +1,22 @@
+import pytest
+import os
 from ..launcher import Launcher
 
 class TestRunProgram:
+    
+    @pytest.fixture(scope='module')
+    def create_test_file(self, request):
+        code='''
+        print([i**2 for i in range(20)])
+        '''
+        filepath=os.path.join('..','test_code.py')
+        with open(filepath, mode='w') as file:
+            file.write(code)
+        def teardown():
+            os.remove(filepath)
+        request.addfinalizer(teardown)
+        return create_test_file
+    
     def test_run(self):
         l = Launcher("")
         l.run()
