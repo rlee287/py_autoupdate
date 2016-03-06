bc290901750d2c60e6261609766dcb3a3eb00a07
rlee287 <rlee287@yahoo.com>
2016-03-06 13:38:05 -0800

    Updated launcher with specifiable path

--- launcher.py
+++ launcher.py
@@ -1,18 +1,28 @@
 import requests
 import multiprocessing
-from .src import start
-from .exceptions import *
+import sys
+#from .src import start
+#from .exceptions import *
 
 class Launcher:
-    def __init__(self, url, **kwargs):
+    def __init__(self, filepath, url, **kwargs):
         self.url=url
+        self.filepath=filepath
         self.updateEvent=multiprocessing.Event()
         self.extraArgs=kwargs
 
+    def call_code(self):
+        try:
+            with open(self.filepath, mode='r') as file:
+                code=file.read()
+                def code_func(updateEvent,**kwargs): pass
+                code_func.__code__=code
+                code_func(self.updateEvent,**self.extraArgs)
+        except IOError:
+            print('Unable to open file to run code', file=sys.stderr)
+    
     def run(self):
-        p = multiprocessing.Process(target=start.run,
-                                    args=(self.updateEvent,),
-                                    kwargs=self.extraArgs)
+        p = multiprocessing.Process(target=self.call_code)
         p.start()
         p.join()
 
