890fa27bab8de9e160cf3e6970eaf814b4de628b
rlee287 <rlee287@yahoo.com>
2016-03-06 19:14:55 -0800

    Updated test_run

--- testing/test_run.py
+++ testing/test_run.py
@@ -4,19 +4,31 @@ from ..launcher import Launcher
 
 class TestRunProgram:
     
-    @pytest.fixture(scope='module')
+    @pytest.fixture(scope='session')
     def create_test_file(self, request):
-        code='''
-        print([i**2 for i in range(20)])
-        '''
-        filepath=os.path.join('..','test_code.py')
-        with open(filepath, mode='w') as file:
+        filebase='codetemp'
+        filecode=filebase+'.py'
+        filetext=filebase+'.txt'
+        code='with open("'+filetext+'", mode="w") as file:\n'+\
+        '    l=[i**2 for i in range(20)]\n'+\
+        '    file.write(str(l))\n'
+        with open(filecode, mode='w') as file:
             file.write(code)
         def teardown():
-            os.remove(filepath)
+            try:
+                os.remove(filecode)
+                os.remove(filetext)
+            except (FileNotFoundError, OSError):
+                raise AssertionError # fail test here
         request.addfinalizer(teardown)
-        return create_test_file
+        return self.create_test_file
     
-    def test_run(self):
-        l = Launcher(os.path.join('..','test_code.py'),'')
+    def test_run(self,create_test_file):
+        filebase='codetemp'
+        filecode=filebase+'.py'
+        filetext=filebase+'.txt'
+        l = Launcher(filecode,'')
         l.run()
+        with open(filetext,mode="r") as file:
+            s=file.read()
+            assert s==str([i**2 for i in range(20)])
