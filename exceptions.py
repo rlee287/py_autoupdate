ecfb0840b2e3fcce93867cc8db8728e001675e10
rlee287 <rlee287@yahoo.com>
2016-03-07 19:20:25 -0800

    Cleaned up obsolete files

--- exceptions.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import, print_function
-
-class UpdaterException (Exception):
-    pass
-
-class StartUpdateException (UpdaterException):
-    pass
--- src/start.py
+++
@@ -1,9 +0,0 @@
-# This is the file that is called by the updater to launch the program
-# Initialization code goes under here in the run() method
-
-import time
-
-def run(UpdateEvent,**kwargs):
-    print("Start")
-    time.sleep(3)
-    print("End")
