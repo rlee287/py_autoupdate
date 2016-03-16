d97efbc55765ce1aeb98dda6963dc8a406e49fd3
rlee287 <rlee287@yahoo.com>
2016-03-16 12:51:12 -0700

    Added mkdir test

---
+++ testing/test_mk_dirs.py
@@ -0,0 +1,25 @@
+from __future__ import absolute_import, print_function
+
+import pytest
+import os
+import sys
+from ..launcher import Launcher
+
+class TestRunProgram:
+    
+    @pytest.fixture(scope='class')
+    def create_update_dir(self, request):
+        def teardown():
+            try:
+                if os.path.isdir('downloads'):
+                    os.rmdir('downloads')
+            except Exception as e:
+                print(e, file=sys.stderr)
+                raise AssertionError
+        request.addfinalizer(teardown)
+        return self.create_update_dir
+    
+    def test_rm_dirs(self,create_update_dir):
+        l = Launcher('','')
+        l._reset_update_dir()
+        assert os.path.isdir('downloads')
