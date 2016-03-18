c9f46d322f777e4d98d8e9a25e50949d308696e0
rlee287 <rlee287@yahoo.com>
2016-03-17 20:11:54 -0700

    Simplified cleanup code for update directory
    (cherry picked from commit af0c8608f72b66bb445474c935584fcc1b41df7c)

--- launcher.py
+++ launcher.py
@@ -34,15 +34,11 @@ class Launcher:
             os.makedirs(self.updatedir)
         else:
             #Remove old contents
-            for files in os.listdir(self.updatedir):
-                file_path = os.path.join(self.updatedir, files)
-                try:
-                    if os.path.isfile(file_path):
-                        os.unlink(file_path)
-                    elif os.path.isdir(file_path):
-                        shutil.rmtree(file_path)
-                except Exception as e:
-                    print(e, file=sys.stderr)
+            try:
+                shutil.rmtree(self.updatedir)
+            except:
+                print(e, file=sys.stderr)
+                
 
     def _get_new(self):
         local_filename = self.url.split('/')[-1]
