20e160dda529a071cddffa53d9027673217993b2
rlee287 <rlee287@yahoo.com>
2016-03-19 16:25:51 -0700

    Adjusted versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import *
 
 setup(
     name='py_autoupdate',
-    version='0.1.0a0.dev0',
+    version='0.0.1.dev0',
     packages=find_packages(),
-    install_requires=['requests>=2.0']
+    install_requires=['requests>=2.6']
 )
