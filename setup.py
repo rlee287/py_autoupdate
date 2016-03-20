ae34bc5e0074d28f6141ae2856e59022785ff7c1
rlee287 <rlee287@yahoo.com>
2016-03-19 17:15:38 -0700

    package data

--- setup.py
+++ setup.py
@@ -5,4 +5,8 @@ setup(
     version='0.0.1.dev0',
     packages=find_packages(),
     install_requires=['requests>=2.6']
+    package_data={
+       '':['*.txt','*.ini','*.md']
+       'testing':['*.md']
+    license="LGPL 2.1"
 )
