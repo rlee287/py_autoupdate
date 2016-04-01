bcc30a4c7fcef85b15ad9ccb73cf9d4f51815f06
rlee287 <rlee287@yahoo.com>
2016-03-31 19:57:13 -0700

    Fixed commas and typos in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import *
 
 setup(
-    name='py_autoupdate',
+    name='pyautoupdate',
     version='0.0.1.dev0',
     packages=find_packages(),
-    install_requires=['requests>=2.6']
+    install_requires=['requests>=2.6'],
     package_data={
-       '':['*.txt','*.ini','*.md']
-       'testing':['*.md']
+       '':['*.txt','*.ini','*.md'],
+       'testing':['*.md']},
     license="LGPL 2.1"
 )
