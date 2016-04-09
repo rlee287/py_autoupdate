3ed27e90767da17d5129c3ca419ef1e8e344bc07
rlee287 <rlee287@yahoo.com>
2016-04-09 11:05:05 -0700

    Still show local vars second time

--- testing/scripts/run_linux.sh
+++ testing/scripts/run_linux.sh
@@ -2,4 +2,4 @@
 echo "Main test"
 py.test -vl -r a --cov=pyautoupdate --cov-report=term-missing ./testing
 echo "See stderr and stdout to rule out false positives"
-py.test -vs
+py.test -vls
--- testing/scripts/run_windows.bat
+++ testing/scripts/run_windows.bat
@@ -2,5 +2,5 @@
 echo Main test
 py.test -vl -r a --cov=pyautoupdate --cov-report=term-missing .\testing
 echo See stderr and stdout to rule out false positives
-py.test -vs
+py.test -vls
 
