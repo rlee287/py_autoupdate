23c04132a992fd93688a4125be8f36f175420ddd
rlee287 <rlee287@yahoo.com>
2016-04-12 22:46:18 -0700

    Add more info to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import *
 
+with open("README.md", mode='r') as file:
+    text=file.read()
+
 setup(
     name='pyautoupdate',
     version='0.0.1.dev0',
     packages=find_packages(),
+    description='Interface to allow python programs to automatically update',
+    long_description=text,
+    url='https://github.com/rlee287/pyautoupdate'
     install_requires=['requests>=2.6'],
     package_data={
        '':['*.txt','*.ini','*.md'],
