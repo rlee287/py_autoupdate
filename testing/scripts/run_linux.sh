a35e24bf323b2db1707b2f5a3a64d92b6aebd79a
rlee287 <rlee287@yahoo.com>
2016-04-15 12:05:43 -0700

    Move pytest options into pytest.ini file

--- testing/scripts/run_linux.sh
+++ testing/scripts/run_linux.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-echo "Main test"
-py.test -vl -r a --cov=pyautoupdate --cov-report=term-missing ./testing
-echo "See stderr and stdout to rule out false positives"
-py.test -vls
+py.test ./testing
