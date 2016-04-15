3a34aed311018b135271cbc544c095914530ff70
rlee287 <rlee287@yahoo.com>
2016-04-15 10:19:48 -0700

    Close socket after connection is attempted

--- testing/pytest_skipif.py
+++ testing/pytest_skipif.py
@@ -5,10 +5,11 @@ def has_internet():
     '''Uses 8.8.8.8 to check connectivity'''
     try:
         socket.setdefaulttimeout(1)
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM)\
-                      .connect(('8.8.8.8',53))
+        test_connection=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        test_connection.connect(('8.8.8.8',53))
+        test_connection.close()
         return True
-    except:
+    except OSError:
         return False
 
 needinternet=pytest.mark.skipif(not has_internet(), reason="This test needs internet")
