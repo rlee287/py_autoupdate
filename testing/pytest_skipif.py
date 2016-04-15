2437d2f321f51f1439cfc5f43a3984e193d1f506
rlee287 <rlee287@yahoo.com>
2016-04-15 10:41:49 -0700

    Reimplemented skipif to use requests instead of sockets

--- testing/pytest_skipif.py
+++ testing/pytest_skipif.py
@@ -1,15 +1,12 @@
-import socket
+import requests
 import pytest
 
 def has_internet():
     '''Uses 8.8.8.8 to check connectivity'''
     try:
-        socket.setdefaulttimeout(1)
-        test_connection=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        test_connection.connect(('8.8.8.8',53))
-        test_connection.close()
+        requests.head('http://www.google.com', timeout=1)
         return True
-    except OSError:
+    except requests.ConnectionError:
         return False
 
 needinternet=pytest.mark.skipif(not has_internet(), reason="This test needs internet")
