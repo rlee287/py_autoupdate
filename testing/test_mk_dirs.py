4948e574dceb4dfbc4f3a661ede1ac4633387595
rlee287 <rlee287@yahoo.com>
2016-04-15 23:20:23 -0700

    Migrate most tests out of classes

--- testing/test_mk_dirs.py
+++ testing/test_mk_dirs.py
@@ -4,17 +4,15 @@ from ..launcher import Launcher
 import os
 import pytest
 
-class TestRunProgram:
-    
-    @pytest.fixture(scope='class')
-    def create_update_dir(self, request):
-        def teardown():
-            if os.path.isdir('downloads'):
-                os.rmdir('downloads')
-        request.addfinalizer(teardown)
-        return self.create_update_dir
-    
-    def test_rm_dirs(self,create_update_dir):
-        l = Launcher('','')
-        l._reset_update_dir()
-        assert os.path.isdir('downloads')
+@pytest.fixture(scope='function')
+def create_update_dir(request):
+    def teardown():
+        if os.path.isdir('downloads'):
+            os.rmdir('downloads')
+    request.addfinalizer(teardown)
+    return create_update_dir
+
+def test_mk_dirs(create_update_dir):
+    l = Launcher('','')
+    l._reset_update_dir()
+    assert os.path.isdir('downloads')
--- testing/test_no_update.py
+++ testing/test_no_update.py
@@ -6,21 +6,20 @@ import pytest
 
 from .pytest_skipif import needinternet
 
-class TestRunProgram:
-    @pytest.fixture(scope='class')
-    def create_update_dir(self, request):
-        def teardown():
-            if os.path.isfile('version.txt.old'):
-                os.remove('version.txt.old')
-            if os.path.isfile('version.txt'):
-                os.remove('version.txt')
-        request.addfinalizer(teardown)
-        with open('version.txt', mode='w') as file:
-            file.write("0.2.0")
-        return self.create_update_dir
+@pytest.fixture(scope='function')
+def create_update_dir(request):
+    def teardown():
+        if os.path.isfile('version.txt.old'):
+            os.remove('version.txt.old')
+        if os.path.isfile('version.txt'):
+            os.remove('version.txt')
+    request.addfinalizer(teardown)
+    with open('version.txt', mode='w') as file:
+        file.write("0.2.0")
+    return create_update_dir
     
-    @needinternet
-    def test_check_vers_noupdate(self,create_update_dir):
-        launch = Launcher('',r'http://rlee287.github.io/pyautoupdate/testing/')
-        isnew=launch.check_new()
-        assert not isnew
+@needinternet
+def test_check_vers_noupdate(create_update_dir):
+    launch = Launcher('',r'http://rlee287.github.io/pyautoupdate/testing/')
+    isnew=launch.check_new()
+    assert not isnew
