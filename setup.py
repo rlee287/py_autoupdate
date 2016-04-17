8d95fa0f82aaabdf8900c0a622e8b45e711b94a8
rlee287 <rlee287@yahoo.com>
2016-04-17 10:32:21 -0700

    Fixed setup.py typo

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=find_packages(),
     description='Interface to allow python programs to automatically update',
     long_description=text,
-    url='https://github.com/rlee287/pyautoupdate'
+    url='https://github.com/rlee287/pyautoupdate',
     install_requires=['requests>=2.6'],
     package_data={
        '':['*.txt','*.ini','*.md'],
