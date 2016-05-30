81b15243d467d8cc06178656b15516ed355d1570
rlee287 <rlee287@yahoo.com>
2016-05-30 08:41:13 -0700

    Fixed tests by adding additional file assertions

--- testing/test_need_update.py
+++ testing/test_need_update.py
@@ -4,9 +4,13 @@ from ..pyautoupdate.launcher import Launcher
 from .pytest_skipif import needinternet
 from .pytest_makevers import fixture_update_dir
 
+import os
+
 @needinternet
 def test_check_vers_update(fixture_update_dir):
     package=fixture_update_dir("0.0.1")
     launch = Launcher('',r'http://rlee287.github.io/pyautoupdate/testing/')
     isnew=launch.check_new()
     assert isnew
+    assert os.path.isfile("version.txt")
+    assert os.path.isfile("version.txt.old")
--- testing/test_no_update.py
+++ testing/test_no_update.py
@@ -4,9 +4,13 @@ from ..pyautoupdate.launcher import Launcher
 from .pytest_skipif import needinternet
 from .pytest_makevers import fixture_update_dir
 
+import os
+
 @needinternet
 def test_check_vers_noupdate(fixture_update_dir):
     package=fixture_update_dir('0.2.0')
     launch = Launcher('',r'http://rlee287.github.io/pyautoupdate/testing/')
     isnew=launch.check_new()
     assert not isnew
+    assert os.path.isfile("version.txt")
+    assert os.path.isfile("version.txt.old")
