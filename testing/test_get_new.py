28d1cf600697527509bdd554e77fe709eaf5bf91
rlee287 <rlee287@yahoo.com>
2016-06-07 21:48:42 -0700

    Updated directory inside get_new test after replacing online file

--- testing/test_get_new.py
+++ testing/test_get_new.py
@@ -14,6 +14,6 @@ def test_check_vers_update(fixture_update_dir):
     package=fixture_update_dir("0.0.1")
     launch = Launcher('',r'http://rlee287.github.io/pyautoupdate/testing/')
     launch._get_new()
-    with open(os.path.abspath("downloads/blah.py"), "r") as file_code:
+    with open(os.path.abspath("downloads/extradir/blah.py"), "r") as file_code:
         file_text=file_code.read()
     assert "new version" in file_text
