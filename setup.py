f6a952c96ab4a6d2df44d6da2c225cd39900acb2
rlee287 <rlee287@yahoo.com>
2016-06-22 20:27:25 -0700

    Used regular expression code from requests to parse version numbers

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup, find_packages
 
+import re
+
 with open("README.rst", mode='r') as readme_file:
     text=readme_file.read()
 
+#below version code pulled from requests module
+with open('__init__.py', 'r') as fd:
+    version_number = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        fd.read(), re.MULTILINE).group(1)
+if not version_number:
+    raise RuntimeError('Cannot find version information')
+
 setup(
     name='pyautoupdate',
-    version='0.0.1.dev0',
+    version=version_number,
     packages=find_packages(),
     description='Interface to allow python programs to automatically update',
     long_description=text,
