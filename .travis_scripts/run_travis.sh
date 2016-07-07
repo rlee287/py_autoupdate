6b47348a167f0d6be63756d741549c21edbaca07
rlee287 <rlee287@yahoo.com>
2016-07-06 22:22:16 -0700

    Switch to coverage.py for coverage and give more diagnostic output

--- .travis_scripts/run_travis.sh
+++ .travis_scripts/run_travis.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 # Run tests
+echo -e "\e[0;34mRunning test suite...\e[0m"
 cd 'test' || exit 1
-py.test --cov-report xml .
+coverage run --source .,../pyautoupdate -m pytest
 test_exit=$?
 cd ..
+echo -e "\e[0;34mDone running tests\e[0m"
 # Ensure that package installs properly
+echo -e "\e[0;35mInstalling package...\e[0m"
 python setup.py develop || exit 1
+echo -e "\e[0;35mAttempting to import package from python...\e[0m"
 python -c "import pyautoupdate"
 install_exit=$?
+echo -e "\e[0;35mCleaning up...\e[0m"
 python setup.py develop --uninstall
 if [ $test_exit = 0 ] && [ $install_exit = 0]; then
     exit 0
