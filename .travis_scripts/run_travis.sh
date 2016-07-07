7d18cf2e7cee03cb308c4d80f90e5fdd1db0780c
rlee287 <rlee287@yahoo.com>
2016-07-06 22:28:46 -0700

    Show coverage even when tests fail

--- .travis_scripts/run_travis.sh
+++ .travis_scripts/run_travis.sh
@@ -4,13 +4,15 @@ echo -e "\e[0;34mRunning test suite...\e[0m"
 cd 'test' || exit 1
 coverage run --source .,../pyautoupdate -m pytest
 test_exit=$?
+coverage report
 cd ..
 echo -e "\e[0;34mDone running tests\e[0m"
 # Ensure that package installs properly
 echo -e "\e[0;35mInstalling package...\e[0m"
 python setup.py develop || exit 1
+echo -e "\e[0;35mDone installing\e[0m"
 echo -e "\e[0;35mAttempting to import package from python...\e[0m"
-python -c "import pyautoupdate"
+python -c "import pyautoupdate" && echo -e "\e[0;32mInstallation successful\e0m]"
 install_exit=$?
 echo -e "\e[0;35mCleaning up...\e[0m"
 python setup.py develop --uninstall
