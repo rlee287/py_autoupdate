a5a98d8bd4f1507988532dcac29a000ba2de53f1
rlee287 <rlee287@yahoo.com>
2016-07-09 16:47:01 -0700

    Renamed urlslash to parameters and added tests for empty files or URLs
    This also forces a codecov report to be uploaded.

--- test/test_parameters.py
+++ test/test_parameters.py
@@ -4,9 +4,19 @@ from ..pyautoupdate.launcher import Launcher
 
 import os
 
+import pytest
+
 def test_check_urlslash():
     launch = Launcher('not here',
                       r'http://rlee287.github.io/pyautoupdate/testing/')
     launch2 = Launcher('why do I need to do this',
                        r'http://rlee287.github.io/pyautoupdate/testing')
     assert launch.url == launch2.url
+
+def test_check_emptyfilepath():
+    with pytest.raises(ValueError):
+        launch = Launcher('','a url')
+
+def test_check_emptyURL():
+    with pytest.raises(ValueError):
+        launch = Launcher('a filepath','')
