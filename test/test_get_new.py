c6447310063a1521d83a4f7fd0b4bc548d54835b
rlee287 <rlee287@yahoo.com>
2016-07-10 16:56:36 -0700

    Add create_zip fixture to get_new test to cover more branches

--- test/test_get_new.py
+++ test/test_get_new.py
@@ -7,10 +7,25 @@ from .pytest_makevers import fixture_update_dir
 import pytest
 
 import os
+import sys
+
+@pytest.fixture("function")
+def create_zip(request):
+    def teardown():
+        if os.path.isfile("project.zip"):
+            os.remove("project.zip")
+    request.addfinalizer(teardown)
+    if sys.version_info[0]==2:
+        empty_zip_data = 'PK\x05\x06\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    else:
+        empty_zip_data = b'PK\x05\x06\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    with open('project.zip', 'wb') as zip:
+        zip.write(empty_zip_data)
+
 
 @pytest.mark.trylast
 @needinternet
-def test_check_vers_update(fixture_update_dir):
+def test_check_vers_update(create_zip, fixture_update_dir):
     package=fixture_update_dir("0.0.1")
     launch = Launcher('filling up the boring replacements',
                       r'http://rlee287.github.io/pyautoupdate/testing/')
