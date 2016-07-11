11d4763b093d0f1006051e892277d33ca273916c
rlee287 <rlee287@yahoo.com>
2016-07-10 23:11:52 -0700

    Fix continuation whitespace and add extras_require

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.rst", mode='r') as readme_file:
 #below version code pulled from requests module
 with open('__init__.py', 'r') as fd:
     version_number = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fd.read(), re.MULTILINE).group(1)
+                               fd.read(), re.MULTILINE).group(1)
 if not version_number:
     raise RuntimeError('Cannot find version information')
 
@@ -19,7 +19,10 @@ setup(
     description='Interface to allow python programs to automatically update',
     long_description=text,
     url='https://github.com/rlee287/pyautoupdate',
-    install_requires=['requests>=2.6'],
+    install_requires=['requests'],
+    extras_require={
+        'testing': ['pytest','coverage']
+    },
     package_data={
         'testing':['*.rst']},
     license="LGPL 2.1"
