48adf9e8b66f3875964c0afc88a0261cec589c26
rlee287 <rlee287@yahoo.com>
2016-07-23 13:26:04 -0700

    Remove unused copy_glob function

--- pyautoupdate/_move_glob.py
+++ pyautoupdate/_move_glob.py
@@ -9,13 +9,3 @@ def move_glob(src,dst):
     for obj in glob.iglob(src):
         shutil.move(obj,dst)
 
-def copy_glob(src,dst):
-    """Copies files from src to dest.
-
-    src may be any glob to recognize files. dst must be a folder."""
-    for obj in glob.iglob(src):
-        if os.path.isdir(obj):
-            shutil.copytree(obj,dst)
-        else:
-            shutil.copy2(obj,dst)
-
