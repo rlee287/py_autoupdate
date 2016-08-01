48283ff88952a51a4d48a210894f62f17bd663cc
rlee287 <rlee287@yahoo.com>
2016-07-31 22:07:11 -0700

    Add copy_glob function

--- pyautoupdate/_move_glob.py
+++ pyautoupdate/_move_glob.py
@@ -1,5 +1,6 @@
 import glob
 import shutil
+import os
 
 def move_glob(src,dst):
     """Moves files from src to dest.
@@ -7,4 +8,17 @@ def move_glob(src,dst):
     src may be any glob to recognize files. dst must be a folder."""
     for obj in glob.iglob(src):
         shutil.move(obj,dst)
+
+def copy_glob(src,dst):
+    """Copies files from src to dest.
+
+    src may be any glob to recognize files. dst must be a folder."""
+    for obj in glob.iglob(src):
+        if os.path.isdir(obj):
+#            start_part=os.path.commonpath([src,obj])
+#            end_part=os.path.relpath(obj,start_part)
+            shutil.copytree(obj,dst)
+        else:
+            shutil.copy2(obj,dst)
+
 
