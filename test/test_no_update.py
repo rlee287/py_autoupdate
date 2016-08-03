0920f06b0719d96c6e544f5dd6e740e6e241fff8
rlee287 <rlee287@yahoo.com>
2016-08-02 23:01:01 -0700

    test_no_update calls higher level update method

---
+++ test/test_no_update.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, print_function
+
+from ..pyautoupdate.launcher import Launcher
+from .pytest_skipif import needinternet
+from .pytest_makevers import fixture_update_dir
+
+import os
+
+@needinternet
+def test_check_vers_noupdate(fixture_update_dir):
+    package=fixture_update_dir('0.2.0')
+    launch = Launcher('pypipypipypipypi',
+                      r'http://rlee287.github.io/pyautoupdate/testing/')
+    isnew=launch.update_code()
