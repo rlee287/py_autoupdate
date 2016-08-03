758a69d63780ce696cca5d94606d75b643acf663
rlee287 <rlee287@yahoo.com>
2016-08-02 21:42:19 -0700

    Remove the backupdir, as it was unnecessary

--- pyautoupdate/launcher.py
+++ pyautoupdate/launcher.py
@@ -224,27 +224,19 @@ class Launcher:
             print("Writing new filelist to filelist.txt")
             with open("filelist.txt", "w") as file_handle:
                 file_handle.writelines(filelist_new)
-            print("Backup tempdir")
-            backupdir=tempfile.mkdtemp()
-            copy_glob(os.path.join(tempdir,"*"),backupdir)
-            print("Move tempdir contents to current directory")
-            move_glob(os.path.join(tempdir,"*"),".")
+            print("Copy tempdir contents to current directory")
+            copy_glob(os.path.join(tempdir,"*"),".")
             print("Remove backup filelist")
             filelist_backup.close()
             os.remove(filelist_backup.name)
             shutil.rmtree(backupdir)
-            print("Remaining files of tempdir")
-            pprint.pprint(os.listdir(tempdir))
+            #print("Remaining files of tempdir")
+            #pprint.pprint(os.listdir(tempdir))
         except Exception:
             raise
         finally:
-            try:
-                os.rmdir(tempdir) #Should be empty at this point unless something happened
-            except OSError:
-                print("Tempdir is not empty!")
-                print("Here are its contents")
-                pprint.pprint(os.listdir(tempdir))
-                shutil.rmtree(tempdir)
+            print("Removing tempdir")
+            shutil.rmtree(tempdir)
 
     def update_code(self):
         if self.check_new():
