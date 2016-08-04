018c7550fcbfc316c88df877e26c81a19eb41474
rlee287 <rlee287@yahoo.com>
2016-08-03 17:48:28 -0700

    Forgot docstring for test_no_need_update

--- test/test_no_need_update.py
+++ test/test_no_need_update.py
@@ -8,6 +8,7 @@ import os
 
 @needinternet
 def test_check_vers_noupdate(fixture_update_dir):
+    """Test to check update checker when update is not necessary"""
     package=fixture_update_dir('0.2.0')
     launch = Launcher('pypipypipypipypi',
                       r'http://rlee287.github.io/pyautoupdate/testing/')
