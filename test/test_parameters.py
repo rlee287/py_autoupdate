0ef3fa0f0103e1223294e4856528355410480f9b
rlee287 <rlee287@yahoo.com>
2016-08-07 10:43:40 -0700

    Add docstrings to test_parameters

--- test/test_parameters.py
+++ test/test_parameters.py
@@ -9,6 +9,7 @@ import warnings
 import pytest
 
 def test_check_urlslash():
+    """Check that slash is appended to end of url"""
     launch = Launcher('not here',
                       r'http://rlee287.github.io/pyautoupdate/testing/')
     launch2 = Launcher('why do I need to do this',
@@ -16,15 +17,18 @@ def test_check_urlslash():
     assert launch.url == launch2.url
 
 def test_check_emptyfilepath():
+    """Check that error is raised with empty file"""
     with pytest.raises(ValueError):
         Launcher('','a url')
 
 def test_check_emptyURL():
+    """Check that error is raised with empty URL"""
     with pytest.raises(ValueError):
         Launcher('a filepath','')
 
 @pytest.fixture(scope="function")
 def fixture_corrupt_log(request):
+    """Fixture that creates corrupted log"""
     with open("version_history.log","w") as log:
         log.write("invalid!gibberish")
     def teardown():
@@ -35,6 +39,7 @@ def fixture_corrupt_log(request):
 
 @pytest.fixture(scope="function")
 def fixture_corrupt_vers(request):
+    """Fixture that creates invalid version file"""
     with open("version.txt","w") as vers_file:
         vers_file.write("invalid?version")
     def teardown():
@@ -44,12 +49,14 @@ def fixture_corrupt_vers(request):
     return fixture_corrupt_vers
 
 def test_check_corrupted_log(fixture_corrupt_log):
+    """Test that checks for corrupted log error"""
     with pytest.raises(CorruptedFileWarning):
         with warnings.catch_warnings():
             warnings.simplefilter("error",category=CorruptedFileWarning)
             launch=Launcher("123","456")
 
 def test_check_corrupted_vers(fixture_corrupt_vers):
+    """Test that checks for corrupted version error"""
     with pytest.raises(CorruptedFileWarning):
         with warnings.catch_warnings():
             warnings.simplefilter("error",category=CorruptedFileWarning)
