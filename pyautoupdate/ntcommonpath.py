fccdb9412f78645bea8ecd4e3e7a4a22f667e4ec
rlee287 <rlee287@yahoo.com>
2016-08-14 23:02:11 -0700

    Fix merging errors for ntcommonpath.py
    Similar thing happened earlier for posixcommonpath.py
    Wasn't caught earlier because Travis CI uses Linux

--- pyautoupdate/ntcommonpath.py
+++ pyautoupdate/ntcommonpath.py
@@ -32,37 +32,37 @@ def commonpath(paths):
         altsep = '/'
         curdir = '.'
 
+    drivesplits = [splitdrive(p.replace(altsep, sep).lower()) for p in paths]
+    split_paths = [p.split(sep) for d, p in drivesplits]
+
     try:
-        drivesplits = [splitdrive(p.replace(altsep, sep).lower()) for p in paths]
-        split_paths = [p.split(sep) for d, p in drivesplits]
+        isabs, = set(p[:1] == sep for d, p in drivesplits)
+    except ValueError:
+        ### Begin modified block ###
+        #raise ValueError("Can't mix absolute and relative paths") from None
+        #raise from None is not supported in Python 2
+        raise ValueError("Can't mix absolute and relative paths")
+        ### End modified block ###
 
-        try:
-            isabs, = set(p[:1] == sep for d, p in drivesplits)
-        except ValueError:
-            ### Begin modified block ###
-            #raise ValueError("Can't mix absolute and relative paths") from None
-            #raise from None is not supported in Python 2
-            raise ValueError("Can't mix absolute and relative paths")
-            ### End modified block ###
+    # Check that all drive letters or UNC paths match. The check is made only
+    # now otherwise type errors for mixing strings and bytes would not be
+    # caught.
+    if len(set(d for d, p in drivesplits)) != 1:
+        raise ValueError("Paths don't have the same drive")
 
-        # Check that all drive letters or UNC paths match. The check is made only
-        # now otherwise type errors for mixing strings and bytes would not be
-        # caught.
-        if len(set(d for d, p in drivesplits)) != 1:
-            raise ValueError("Paths don't have the same drive")
+    drive, path = splitdrive(paths[0].replace(altsep, sep))
+    common = path.split(sep)
+    common = [c for c in common if c and c != curdir]
 
-        drive, path = splitdrive(paths[0].replace(altsep, sep))
-        common = path.split(sep)
-        common = [c for c in common if c and c != curdir]
+    split_paths = [[c for c in s if c and c != curdir] for s in split_paths]
+    s1 = min(split_paths)
+    s2 = max(split_paths)
+    for i, c in enumerate(s1):
+        if c != s2[i]:
+            common = common[:i]
+            break
+    else:
+        common = common[:len(s1)]
 
-        split_paths = [[c for c in s if c and c != curdir] for s in split_paths]
-        s1 = min(split_paths)
-        s2 = max(split_paths)
-        for i, c in enumerate(s1):
-            if c != s2[i]:
-                common = common[:i]
-                break
-        else:
-            common = common[:len(s1)]
     prefix = drive + sep if isabs else drive
     return prefix + sep.join(common)
