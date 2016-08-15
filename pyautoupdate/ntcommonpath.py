60ac92197e6f893a74a42bb469c64682a16bba35
rlee287 <rlee287@yahoo.com>
2016-08-14 20:12:53 -0700

    Revert "Remove input checks from commonpath backports"
    This reverts commit 9ea157625d0ee736f38ea17a8f6a47080f3fb240.
    
    Still removing genericpath stuff for Python 2

--- pyautoupdate/ntcommonpath.py
+++ pyautoupdate/ntcommonpath.py
@@ -1,6 +1,7 @@
 ###########################################################################
 ### CODE BELOW COPIED FROM Python 3.5.1                                 ###
 ### Backport of ntpath.commonpath functionality                         ###
+### Modifications clearly marked via comments                           ###
 ### CODE USED IN COMPLIANCE WITH https://docs.python.org/3/license.html ###
 ###########################################################################
 
@@ -18,8 +19,9 @@ from ntpath import splitdrive
 
 def commonpath(paths):
     """Given a sequence of path names, returns the longest common sub-path."""
-    #if not paths:
-    #    raise ValueError('commonpath() arg is an empty sequence')
+
+    if not paths:
+        raise ValueError('commonpath() arg is an empty sequence')
 
     if isinstance(paths[0], bytes):
         sep = b'\\'
@@ -30,35 +32,37 @@ def commonpath(paths):
         altsep = '/'
         curdir = '.'
 
-    drivesplits = [splitdrive(p.replace(altsep, sep).lower()) for p in paths]
-    split_paths = [p.split(sep) for d, p in drivesplits]
-
     try:
-        isabs, = set(p[:1] == sep for d, p in drivesplits)
-    except ValueError:
-        #raise ValueError("Can't mix absolute and relative paths") from None
-        #raise from None is not supported in Python 2
-        raise ValueError("Can't mix absolute and relative paths")
+        drivesplits = [splitdrive(p.replace(altsep, sep).lower()) for p in paths]
+        split_paths = [p.split(sep) for d, p in drivesplits]
 
-    # Check that all drive letters or UNC paths match. The check is made only
-    # now otherwise type errors for mixing strings and bytes would not be
-    # caught.
-    if len(set(d for d, p in drivesplits)) != 1:
-        raise ValueError("Paths don't have the same drive")
+        try:
+            isabs, = set(p[:1] == sep for d, p in drivesplits)
+        except ValueError:
+            ### Begin modified block ###
+            #raise ValueError("Can't mix absolute and relative paths") from None
+            #raise from None is not supported in Python 2
+            raise ValueError("Can't mix absolute and relative paths")
+            ### End modified block ###
 
-    drive, path = splitdrive(paths[0].replace(altsep, sep))
-    common = path.split(sep)
-    common = [c for c in common if c and c != curdir]
+        # Check that all drive letters or UNC paths match. The check is made only
+        # now otherwise type errors for mixing strings and bytes would not be
+        # caught.
+        if len(set(d for d, p in drivesplits)) != 1:
+            raise ValueError("Paths don't have the same drive")
 
-    split_paths = [[c for c in s if c and c != curdir] for s in split_paths]
-    s1 = min(split_paths)
-    s2 = max(split_paths)
-    for i, c in enumerate(s1):
-        if c != s2[i]:
-            common = common[:i]
-            break
-    else:
-        common = common[:len(s1)]
+        drive, path = splitdrive(paths[0].replace(altsep, sep))
+        common = path.split(sep)
+        common = [c for c in common if c and c != curdir]
 
+        split_paths = [[c for c in s if c and c != curdir] for s in split_paths]
+        s1 = min(split_paths)
+        s2 = max(split_paths)
+        for i, c in enumerate(s1):
+            if c != s2[i]:
+                common = common[:i]
+                break
+        else:
+            common = common[:len(s1)]
     prefix = drive + sep if isabs else drive
     return prefix + sep.join(common)
--- pyautoupdate/posixcommonpath.py
+++ pyautoupdate/posixcommonpath.py
@@ -1,6 +1,7 @@
 ###########################################################################
 ### CODE BELOW COPIED FROM Python 3.5.1                                 ###
 ### Backport of posixpath.commonpath functionality                      ###
+### Modifications clearly marked via comments                           ###
 ### CODE USED IN COMPLIANCE WITH https://docs.python.org/3/license.html ###
 ###########################################################################
 
@@ -13,6 +14,9 @@
 def commonpath(paths):
     """Given a sequence of path names, returns the longest common sub-path."""
 
+    if not paths:
+        raise ValueError('commonpath() arg is an empty sequence')
+
     if isinstance(paths[0], bytes):
         sep = b'/'
         curdir = b'.'
@@ -20,25 +24,25 @@ def commonpath(paths):
         sep = '/'
         curdir = '.'
 
-    split_paths = [path.split(sep) for path in paths]
-
     try:
-        isabs, = set(p[:1] == sep for p in paths)
-    except ValueError:
-        ### Begin modified block
-        #raise ValueError("Can't mix absolute and relative paths") from None
-        #raise from None is not supported in Python 2
-        raise ValueError("Can't mix absolute and relative paths")
-        ### End modified block
+        split_paths = [path.split(sep) for path in paths]
+        try:
+            isabs, = set(p[:1] == sep for p in paths)
+        except ValueError:
+            ### Begin modified block
+            #raise ValueError("Can't mix absolute and relative paths") from None
+            #raise from None is not supported in Python 2
+            raise ValueError("Can't mix absolute and relative paths")
+            ### End modified block
+        split_paths = [[c for c in s if c and c != curdir] for s in split_paths]
+        s1 = min(split_paths)
+        s2 = max(split_paths)
+        common = s1
+        for i, c in enumerate(s1):
+            if c != s2[i]:
+                common = s1[:i]
+                break
 
-    split_paths = [[c for c in s if c and c != curdir] for s in split_paths]
-    s1 = min(split_paths)
-    s2 = max(split_paths)
-    common = s1
-    for i, c in enumerate(s1):
-        if c != s2[i]:
-            common = s1[:i]
-            break
+        prefix = sep if isabs else sep[:0]
+        return prefix + sep.join(common)
 
-    prefix = sep if isabs else sep[:0]
-    return prefix + sep.join(common)
