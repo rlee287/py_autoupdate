212f5bc088caff647a61e8ba70f8fe0a37fc3741
rlee287 <rlee287@yahoo.com>
2016-08-19 21:48:19 -0700

    Consolidate update checking tests into one file

---
+++ test/test_check_update.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import, print_function
+
+from ..pyautoupdate.launcher import Launcher
+from .pytest_skipif import needinternet
+from .pytest_makevers import fixture_update_dir
+
+import os
+
+import pytest
+from requests import HTTPError
+
+@needinternet
+def test_check_update_needed(fixture_update_dir):
+    """Test that ensures that updates occur when needed"""
+    package=fixture_update_dir("0.0.1")
+    launch = Launcher('blah',
+                      r'http://rlee287.github.io/pyautoupdate/testing/')
+    isnew=launch.check_new()
+    assert isnew
+    assert os.path.isfile("version.txt")
+    assert os.path.isfile("version_history.log")
+    with open("version_history.log","r") as log_handle:
+        log=log_handle.read()
+    assert "New" in log
+
+@needinternet
+def test_check_update_notneeded(fixture_update_dir):
+    """Test to check update checker when update is not necessary"""
+    package=fixture_update_dir('0.2.0')
+    launch = Launcher('pypipypipypipypi',
+                      r'http://rlee287.github.io/pyautoupdate/testing/')
+    isnew=launch.check_new()
+    assert not isnew
+    assert os.path.isfile("version.txt")
+    assert os.path.isfile("version_history.log")
+    with open("version_history.log","r") as log_handle:
+        log=log_handle.read()
+    assert "Up to date" in log
+
+@needinternet
+def test_check_update_nourl(fixture_update_dir):
+    """Test that ensures graceful failure when version.txt is missing"""
+    package=fixture_update_dir("0.2.0")
+    with pytest.raises(HTTPError):
+        #No version.txt at the following url
+        launch = Launcher('ANNOYING',
+                          r'http://rlee287.github.io/pyautoupdate/')
+        launch.check_new()
