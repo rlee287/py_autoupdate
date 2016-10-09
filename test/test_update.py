bf88eb3c9cc7b17ec22ed3a390db49a4a508c89e
rlee287 <rlee287@yahoo.com>
2016-10-08 22:48:26 -0700

    Test locking mechanism with new test that attempts update while code runs

--- test/test_update.py
+++ test/test_update.py
@@ -6,6 +6,7 @@ from .pytest_skipif import needinternet
 from logging import DEBUG
 import os
 import shutil
+import time
 
 import pytest
 
@@ -23,12 +24,20 @@ def fixture_update_setup(request):
             os.remove("filelist.txt")
         if os.path.isdir("downloads"):
             shutil.rmtree("downloads")
+        if os.path.isfile(".queue"):
+            os.remove(".queue")
     request.addfinalizer(teardown)
     with open('version.txt', mode='w') as version_file:
         version_file.write("0.0.1")
     os.mkdir("extradir")
     with open(os.path.join("extradir", "blah.py"), mode='w') as code:
-        code.write("print('This is the old version')")
+        code.write("import time\n"
+                   "import os\n"
+                   "print('This is the old version')\n"
+                   "q=open('.lck','w')\n"
+                   "time.sleep(1)\n"
+                   "q.close()\n"
+                   "os.remove('.lck')\n")
     with open(os.path.join("extradir","dummy.txt"), mode='w') as extra_file:
         extra_file.write("1984: 2+2=5")
     with open("filelist.txt", mode='w') as filelist:
@@ -43,9 +52,30 @@ def test_update(fixture_update_setup):
     launch = Launcher('extradir/blah.py',
                       r'http://rlee287.github.io/pyautoupdate/testing/',
                       'project.zip','downloads',DEBUG)
-    launch.update_code()
+    could_update=launch.update_code()
+    assert could_update
     assert os.path.isfile("extradir/blah.py")
     with open(os.path.abspath("extradir/blah.py"), "r") as file_code:
         file_text=file_code.read()
     assert "new version" in file_text
     assert os.path.isfile("filelist.txt")
+    excode=launch.run()
+    assert excode==0
+
+@pytest.mark.trylast
+@needinternet
+def test_run_lock_update(fixture_update_setup):
+    """Checks the ability of program to upload new code"""
+    assert os.path.isfile("filelist.txt")
+    launch = Launcher('extradir/blah.py',
+                      r'http://rlee287.github.io/pyautoupdate/testing/',
+                      'project.zip','downloads',DEBUG)
+    launch.run(True)
+    while not os.path.isfile(".lck"): # Wait for process to start
+        pass
+    could_update_while_run=launch.update_code()
+    assert not could_update_while_run
+    launch.process_join()
+    could_update=launch.update_code()
+    assert could_update
+    assert os.path.isfile("extradir/blah.py")
