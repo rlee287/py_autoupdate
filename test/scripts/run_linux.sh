14421fbcb0209da444c0d75fa35d34cd4e9d75c4
rlee287 <rlee287@yahoo.com>
2016-11-10 12:46:11 -0800

    Erase coverage after reporting in scripts

--- test/scripts/run_linux.sh
+++ test/scripts/run_linux.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-pytest .coverage run --parallel-mode --source 'test,pyautoupdate' -m pytest
+coverage run --parallel-mode --source 'test,pyautoupdate' -m pytest
 coverage combine
 coverage report -m
+coverage erase
--- test/scripts/run_windows.bat
+++ test/scripts/run_windows.bat
@@ -1,4 +1,5 @@
 @echo off
-pytest .coverage run --parallel-mode --source 'test,pyautoupdate' -m pytest
+coverage run --parallel-mode --source 'test,pyautoupdate' -m pytest
 coverage combine
 coverage report -m
+coverage erase
