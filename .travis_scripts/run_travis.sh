395611da163e8e1cf61c9be4fd34f88576b88e71
rlee287 <rlee287@yahoo.com>
2017-02-27 19:56:36 -0800

    Print explicit messages when setup/import fails

--- .travis_scripts/run_travis.sh
+++ .travis_scripts/run_travis.sh
@@ -14,11 +14,12 @@ python setup.py develop || exit 1
 initialize_exit=1
 echo -e "\e[0;35mDone installing\e[0m"
 echo -e "\e[0;34mAttempting to import package from python...\e[0m"
-python -c "import pyautoupdate" && echo -e "\e[0;32mimport successful\e[0m"
+python -c "import pyautoupdate" && echo -e "\e[0;32mImport successful\e[0m" || echo -e "\e[0;31mImport failed\e[0m"
 install_exit=$?
 if [ $install_exit = 0 ]; then
     echo -e "\e[0;34mInitializing launcher instance\e[0m"
-    python -c "from pyautoupdate.launcher import Launcher;q=Launcher('file.py','URL')" && echo -e "\e[0;32mInitialization successful\e[0m"
+    python -c "from pyautoupdate.launcher import Launcher;q=Launcher('file.py','URL')" \
+        && echo -e "\e[0;32mInitialization successful\e[0m" || echo -e "\e[0;31mInitialization failed\e[0m"
 echo -e "\e[0;35mCleaning up...\e[0m"
     initialize_exit=$?
 fi
