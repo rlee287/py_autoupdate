281cc2f32d2306c8f378ea99ec71ae69c43095f4
rlee287 <rlee287@yahoo.com>
2017-07-10 16:03:34 -0700

    Fix whitespace in test_replace_files

--- test/test_replace_files.py
+++ test/test_replace_files.py
@@ -28,10 +28,10 @@ def fixture_update_setup(request):
     with open(Launcher.queue_update, mode='w') as new_version:
         new_version.write("0.1.0")
     os.mkdir("extradir")
-    os.makedirs(os.path.join(Launcher.updatedir,"extradir"))
-    extradir_blah=os.path.join("extradir","blah.py")
-    extradir_dummy=os.path.join("extradir","dummy.txt")
-    downloads_extradir_blah=os.path.join(Launcher.updatedir,"extradir",
+    os.makedirs(os.path.join(Launcher.updatedir, "extradir"))
+    extradir_blah=os.path.join("extradir", "blah.py")
+    extradir_dummy=os.path.join("extradir", "dummy.txt")
+    downloads_extradir_blah=os.path.join(Launcher.updatedir, "extradir",
                                          "blah.py")
     with open(extradir_blah, mode='w') as code:
         code.write("print('This is the old version')")
@@ -39,7 +39,7 @@ def fixture_update_setup(request):
         extra_file.write("1984: 2+2=5")
     with open(downloads_extradir_blah, mode='w') as new_code:
         new_code.write("print('This is the new version')")
-    list_files=[extradir_blah+"\n",extradir_dummy+"\n","shine/johnny.txt\n"]
+    list_files=[extradir_blah+"\n", extradir_dummy+"\n", "shine/johnny.txt\n"]
     with open(Launcher.file_list, mode='w') as filelist:
         filelist.writelines(list_files)
     return fixture_update_setup
@@ -49,12 +49,12 @@ def test_replace_files(fixture_update_setup):
     assert os.path.isfile(Launcher.file_list)
     launch = Launcher('extradir/blah.py',
                       r'http://rlee287.github.io/pyautoupdate/testing/',
-                      'project.zip',DEBUG)
-    can_replace=launch._replace_files()
+                      'project.zip', DEBUG)
+    can_replace = launch._replace_files()
     assert can_replace
     assert os.path.isfile("extradir/blah.py")
     with open(os.path.abspath("extradir/blah.py"), "r") as file_code:
-        file_text=file_code.read()
+        file_text = file_code.read()
     assert "new version" in file_text
     assert os.path.isfile(Launcher.file_list)
     assert not os.path.isfile(Launcher.queue_update)
@@ -65,6 +65,6 @@ def test_no_replace_files():
     """
     launch = Launcher('extradir/blah.py',
                       r'http://rlee287.github.io/pyautoupdate/testing/',
-                      'project.zip',DEBUG)
-    can_replace=launch._replace_files()
+                      'project.zip', DEBUG)
+    can_replace = launch._replace_files()
     assert not can_replace
