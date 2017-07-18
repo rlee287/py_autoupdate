7f24d458d4953542ad481920642016f482978009
rlee287 <rlee287@yahoo.com>
2017-07-17 20:15:26 -0700

    Fix whitespace in file_glob and attempt coverage modification

--- pyautoupdate/_file_glob.py
+++ pyautoupdate/_file_glob.py
@@ -4,7 +4,7 @@ import os
 
 if os.name == "nt": # pragma: no branch
     from .ntcommonpath import commonpath
-else:
+else: # pragma: no branch
     from .posixcommonpath import commonpath
 
 # def move_glob(src,dst):
@@ -15,19 +15,19 @@ else:
 #     for obj in glob.iglob(src):
 #         shutil.move(obj,dst)
 
-def copy_glob(src,dst):
+def copy_glob(src, dst):
     """Copies files from src to dest.
 
     src may be any glob to recognize files. dst must be a folder.
     """
     for obj in glob.iglob(src):
         if os.path.isdir(obj):
-            start_part=commonpath([src,obj])
-            end_part=os.path.relpath(obj,start_part)
-            ctree_dst=os.path.join(dst,end_part)
+            start_part = commonpath([src, obj])
+            end_part = os.path.relpath(obj, start_part)
+            ctree_dst = os.path.join(dst, end_part)
             if not os.path.isdir(ctree_dst):
-                shutil.copytree(obj,ctree_dst)
+                shutil.copytree(obj, ctree_dst)
             else:
-                copy_glob(os.path.join(obj,"*"),ctree_dst)
+                copy_glob(os.path.join(obj, "*"), ctree_dst)
         else:
-            shutil.copy2(obj,dst)
+            shutil.copy2(obj, dst)
