602f77be2f0036a54e066a7f11d34489e2f67ad9
rlee287 <rlee287@yahoo.com>
2017-07-19 16:17:02 -0700

    Remove now-unnecessary pytest version prints
    Log outdated stuff into temp file to avoid broken pipe errors

--- .travis_scripts/upgrade_pytest.sh
+++ .travis_scripts/upgrade_pytest.sh
@@ -1,8 +1,10 @@
-pip list --outdated | grep -q pytest
+pip list --outdated > temp_pip_outdated.txt
+grep -q pytest temp_pip_outdated.txt
 if [ $? -eq 0 ]; then # match found
   echo "Upgrading pytest"
   pip install --upgrade pytest
 else
   echo "Not upgrading pytest"
 fi
+rm temp_pip_outdated.txt
 
