b736168baebb92efa1db95e57f4883d98d53dc5c
rlee287 <rlee287@yahoo.com>
2017-07-19 16:05:00 -0700

    Fix duplicate pip calls and add echoing

--- .travis_scripts/upgrade_pytest.sh
+++ .travis_scripts/upgrade_pytest.sh
@@ -1,6 +1,8 @@
 pip list --outdated | grep -q pytest
 if [ $? -eq 0 ]; then # match found
+  echo "Upgrading pytest"
   pip install --upgrade pytest
+else
+  echo "Not upgrading pytest"
 fi
-pip install --upgrade pytest
 
