54d5d077258d84578602ad46ae53d1f049d9962c
TravisCIDocBuild <travis_build@nonexistent.email>
2019-03-06 20:41:54 +0000

    Sphinx rebuild
    This corresponds to commit e0ab5ba on branch develop

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,9 +1,10 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.0.0.post1+432ab67',
+    VERSION: '1.0.0.post1+e0ab5ba',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
-    SOURCELINK_SUFFIX: '.txt'
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
 };
